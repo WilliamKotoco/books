f18a628fdefe3a9fa9d43e86912be577229dcc56
William Martins <williamdabruzzmartins76136@gmail.com>
2023-02-05 14:36:13 -0300

    finishing cassino

--- java-como-programar/chapter6/Play.java
+++ java-como-programar/chapter6/Play.java
@@ -7,15 +7,71 @@ import java.util.Scanner;
 
 
 public class Play{
-    public static void main(String[] args)
+    public static void main(String[] args)  throws InterruptedException
     {
         Player player = new Player();
+        Games games = new Games();
         player.starting();  
-        int op;
+        int op, received;
+        boolean receivedStatus;
         while (player.currentMoney > -1000 && player.end == false)
         {
         op = displayInfo(player);
+        switch(op)
+        {
+            case 1:
+            received = games.crazyRoulette();
+            System.out.printf("\n You got the number: %d \n", received);
+            player.currentMoney += received;
+            break;
+
+            case 2:
+            receivedStatus = games.russianRoulette();
+            if (receivedStatus == true)
+            {
+                System.out.println("\n You died!");
+                player.end = receivedStatus;
+            }
+            else
+            {
+                System.out.println("\n You got lucky!");
+                player.currentMoney += 1000;
+            }
+            break;
+
+            case 3:
+            received = games.jokenpo();
+            if (received > 0 )
+                System.out.println("You won!");
+            else if (received == 0)
+                System.out.println("Tied!");
+            else
+                System.out.println("You lost!");
+            player.currentMoney += received;
+            break;
+
+            case 4:
+            received = games.slotMachine();
+            System.out.println("You received " + received);
+            player.currentMoney += received;
+            break;
+
+            case 5:
+            if (player.currentMoney >= 10000)
+            {
+                    System.out.println("You bought the cassino!");
+                    player.end = true;
+            }
+            else
+                System.out.println("Not enough money!");
+            break;
+            
         }
+
+        Thread.sleep(1000);
+        }
+        if(player.currentMoney < 1000)
+            System.out.println("You got killed due your debt with the cassino");
     }
     private static int displayInfo(Player player)
     {
@@ -23,13 +79,14 @@ public class Play{
         int op;
         System.out.println("--------------------------------");
         System.out.println("Displaying information: ");
-        System.out.println("Current Money: " + player.currentMoney);
+        System.out.println("Current Money: " + player.currentMoney + "$");
         // ele converte o current money para string e mostra como string
         System.out.println("--------------------------------");
         System.out.println("[1] - crazy roulette wheel");
         System.out.println("[2] - russian roulette");
         System.out.println("[3] - jokenpo");
-        System.out.println("[4] - cassino");
+        System.out.println("[4] - slot machine");
+        System.out.println("[5] - buy the cassino $10000");
         op = input.nextInt();
         return op;
        
